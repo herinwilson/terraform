d09922567deff0bcdb7bcdec1a100ea740120666
“herinwilson” <herinwilson1@gmail.com>
2020-04-03 22:49:09 +0530

    variables

--- provider.tf
+++ provider.tf
@@ -1,3 +1,22 @@
 provider "aws" {
-  region = "ap-south-1"
+  region  = "${var.region}"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "${var.vpc_cider}"
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "mumbai_vpc"
+  }
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${var.sub_cider}"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Mumbai_subnet1"
+  }
 }
---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "region" {
+default = "ap-south-1"
+}
+
+variable "vpc_cider" {
+default = "10.2.0.0/16"
+}
+
+variable "sub_cider" {
+default = "10.2.1.0/24"
+}
